292e9e57f5b33cf5404075598d20508f99232b4f
CemXokenc <mohikancem@gmail.com>
2026-09-17 00:21:22 +0300

    Solution

--- services/user.py
+++ services/user.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 
-
-User = get_user_model()
+from db.models import User
 
 
 def create_user(
@@ -11,7 +10,7 @@ def create_user(
         first_name: str | None = None,
         last_name: str | None = None
 ) -> User:
-    user = User.objects.create_user(
+    user = get_user_model().objects.create_user(
         username=username,
         password=password
     )
@@ -29,7 +28,7 @@ def create_user(
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.get(pk=user_id)
+    return get_user_model().objects.get(pk=user_id)
 
 
 def update_user(
